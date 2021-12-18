fdf47466d43502720069989145fbda6f3de23273
Wallace Gonçalves <wallacecosme@gmail.com>
2021-12-18 18:36:26 -0300

    Setup inicial do projeto

---
+++ src/main/java/br/com/riachuelo/api/starwars/ApiStarWarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.riachuelo.api.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStarWarsApplication.class, args);
+	}
+
+}
